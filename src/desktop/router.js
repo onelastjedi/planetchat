2e672adf0237a24ae5e614650f6ba3a317c11d3c
Anton Komarenko <mi3ta@sent.as>
2018-09-19 16:34:51 +0200

    :sparkles: Adds basic 'router' for desktop version

---
+++ src/desktop/router.js
@@ -0,0 +1,130 @@
+import Vue from "vue";
+import Router from "vue-router";
+// import { requireAuth, rejectAuth } from "@/utils/router-hooks";
+
+Vue.use(Router);
+
+const loadView = view => {
+  return () =>
+    import(/* webpackChunkName: "view-[request]" */ `@/desktop/views/${view}.vue`);
+};
+
+export default new Router({
+  mode: "history",
+  /* eslint-disable-next-line */
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  },
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: loadView("Home"),
+      // beforeEnter: requireAuth,
+      meta: {
+        isFooter: false,
+        isNotifier: true
+      }
+    },
+//     {
+//       path: "/create",
+//       name: "create",
+//       component: loadView("Create"),
+//       beforeEnter: requireAuth,
+//       meta: {
+//         isFooter: false,
+//         isNotifier: true
+//       }
+//     },
+//     {
+//       path: "/groups/:id",
+//       name: "group",
+//       component: loadView("Group"),
+//       beforeEnter: requireAuth,
+//       meta: {
+//         isFooter: false,
+//         isNotifier: true
+//       }
+//     },
+//     {
+//       path: "/settings",
+//       name: "settings",
+//       component: loadView("Settings"),
+//       beforeEnter: requireAuth,
+//       meta: {
+//         isFooter: false,
+//         isNotifier: true
+//       }
+//     },
+//     {
+//       path: "/settings/details",
+//       name: "details",
+//       component: loadView("Details"),
+//       beforeEnter: requireAuth,
+//       meta: {
+//         isFooter: false,
+//         isNotifier: true
+//       }
+//     },
+//     {
+//       path: "/settings/privacy",
+//       name: "privacy",
+//       component: loadView("Privacy"),
+//       beforeEnter: requireAuth,
+//       meta: {
+//         isFooter: false,
+//         isNotifier: true
+//       }
+//     },
+//     {
+//       path: "/settings/info",
+//       name: "info",
+//       component: loadView("Info"),
+//       beforeEnter: requireAuth,
+//       meta: {
+//         isFooter: false,
+//         isNotifier: true
+//       }
+//     },
+//     {
+//       path: "/login",
+//       name: "login",
+//       component: loadView("Login"),
+//       beforeEnter: rejectAuth,
+//       meta: {
+//         isFooter: true,
+//         isNotifier: false
+//       }
+//     },
+//     {
+//       path: "/signup",
+//       name: "signup",
+//       component: loadView("Signup"),
+//       beforeEnter: rejectAuth,
+//       meta: {
+//         isFooter: true,
+//         isNotifier: false
+//       }
+//     },
+//     {
+//       path: "/forgot",
+//       name: "forgot",
+//       component: loadView("Forgot"),
+//       beforeEnter: rejectAuth,
+//       meta: {
+//         isFooter: true,
+//         isNotifier: false
+//       }
+//     },
+//     {
+//       path: "/reset",
+//       name: "reset",
+//       component: loadView("Reset"),
+//       beforeEnter: rejectAuth,
+//       meta: {
+//         isFooter: true,
+//         isNotifier: false
+//       }
+//     }
+  ]
+});
