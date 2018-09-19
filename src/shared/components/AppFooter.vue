8bfc06f0402198aacd863087b505c2a2793b9fee
Anton Komarenko <mi3ta@sent.as>
2018-09-19 17:13:07 +0200

    :sparkles: Adds shared <AppFooter> component

---
+++ src/shared/components/AppFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer :class="$style.footer">
+    &copy; PlanetChat {{ currentYear }}.&nbsp;&nbsp;<a href="/">View our Data Policy and Terms</a>
+  </footer>
+</template>
+
+<script>
+/**
+ * @file Holds app's footer
+ * @since 0.1.0
+ * @author Anton Komarenko <mi3ta@sent.as>
+ */
+export default {
+  data: () => ({
+    /* Returns current year */
+    currentYear: new Date().getFullYear()
+  })
+};
+</script>
+
+<style module lang="stylus">
+  .footer
+    position relative
+    padding 20px 0
+    text-align center
+    color #2A2E31
+    background white
+    grid-row-start 2
+    grid-row-end 3
+
+    a
+      border-bottom 1px solid #578fc057
+</style>
