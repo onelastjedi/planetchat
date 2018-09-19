c0f53bee05519dfc44a2cf0bb2f4ba6d962893dc
Anton Komarenko <mi3ta@sent.as>
2018-09-19 17:12:28 +0200

    :sparkles: Adds <ChatMessagesEmpty> component

---
+++ src/desktop/components/messages/ChatMessagesEmpty.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="$style.empty">
+    <p>Select a group to start messaging</p>
+  </div>
+</template>
+
+<style module lang="stylus">
+  .empty
+    border-top-left-radius 15px
+    border-top-right-radius 15px
+    text-align center
+    display grid
+    position relative
+    background white
+    align-items center
+
+    @media (max-width: 960px)
+      border-radius 0
+      border-left 1px solid rgba(235,241,245,0.6);
+      height calc(100vh - 80px)
+</style>
