6a237faad6b6cec7423bbe12985b9955f489d1ab
Anton Komarenko <mi3ta@sent.as>
2018-09-19 17:13:27 +0200

    :sparkles: Adds shared <AppStoreBadge> component

---
+++ src/shared/components/AppStoreBadge.vue
@@ -0,0 +1,16 @@
+<template>
+  <a :class="$style.badge" href="/">
+    <img src="@/shared/assets/app-store-badge.svg" />
+  </a>
+</template>
+
+<style module lang="stylus">
+  .badge
+    margin-top 30px
+    margin-bottom 30px
+    display block
+    text-align center
+
+    img
+      width 150px
+</style>
