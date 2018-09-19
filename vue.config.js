8faece9f62f42627997adefa8766a638090916a0
Anton Komarenko <mi3ta@sent.as>
2018-09-19 16:32:04 +0200

    :tada: Initial commit

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  lintOnSave: false,
+  baseUrl: undefined,
+  outputDir: "./desktop/dist",
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined,
+  css: undefined
+};
