8cfafa02084a450121dddcedce5cf1733159887a
Anton Komarenko <mi3ta@sent.as>
2018-09-19 16:34:14 +0200

    :sparkles: Adds <App> for desktop version

---
+++ src/desktop/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main id="app">
+    <!-- <notifier />
+    <full-screen-image-popup />
+    <add-new-contact-popup />
+    <weather-popup />
+    <create-group-popup />
+    <edit-group-popup />
+    <add-group-members-popup />
+    <user-added-popup /> -->
+    <router-view />
+    <!-- <AppFooter v-if="isFooter" /> -->
+  </main>
+</template>
+
+<style lang="stylus" src="@/shared/styles/app.styl" />
