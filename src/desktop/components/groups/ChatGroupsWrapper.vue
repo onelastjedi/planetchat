4c2d1837f6b475e61d3ff666c8bf34f8dc9cee0f
Anton Komarenko <mi3ta@sent.as>
2018-09-19 17:12:11 +0200

    :sparkles: Adds <ChatGroupsWrapper> component

---
+++ src/desktop/components/groups/ChatGroupsWrapper.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :class="$style.chatGroups">
+    <chat-groups-header />
+    <chat-groups-search @search="handleSearchString" />
+    <div :class="$style.chatGroupsWrapper">
+      <chat-groups-item
+        v-for="(group, index) in searched(groups, searchString)"
+        :key="`contacts-${index}`"
+        :last-message-id="group.mmx"
+        :unread-messages-count="group.new"
+        :group-id="group.gid"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    ChatGroupsHeader: () => import("@/desktop/components/groups/ChatGroupsHeader"),
+    ChatGroupsSearch: () => import("@/desktop/components/groups/ChatGroupsSearch"),
+    ChatGroupsItem: () => import("@/desktop/components/groups/ChatGroupsItem")
+  },
+  data: () => ({
+    searchString: ""
+  }),
+  computed: {
+    groups() {
+      return this.$store.state.groups;
+    }
+  },
+  methods: {
+    handleSearchString(event) {
+      this.searchString = event;
+    },
+    searched(groups, searchString) {
+      return this.$_
+        .orderBy(groups, ["messages", "messages.0.sd"], ["desc", "desc"])
+        .filter(
+          group =>
+            group.name && searchString
+              ? group.name.toLowerCase().includes(searchString.toLowerCase())
+              : group
+        );
+    }
+  }
+};
+</script>
+
+<style module lang="stylus">
+  .chatGroups
+    position relative
+    display grid
+    grid-template-rows 50px 60px auto
+    height calc(100vh - 100px)
+
+    @media (max-width: 960px)
+      height calc(100vh - 80px)
+
+    .chatGroupsWrapper
+      overflow auto
+      align-items baseline
+      background white
+</style>
