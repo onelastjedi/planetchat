e3b7b4721a5d1c61bbd09391ee38cebeb92fd2a0
Anton Komarenko <mi3ta@sent.as>
2018-09-23 13:15:28 +0300

    :sparkles: Adds mobile <App>

---
+++ src/mobile/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <main id="app"
+    v-if="isConnected"
+    :class="{ overlayed: isAppOverlayed }"
+  >
+    <AppHeader v-if="isHeader" />
+    <notifier />
+    <full-screen-image-popup />
+    <add-new-contact-popup />
+    <weather-popup />
+    <create-group-popup />
+    <edit-group-popup />
+    <add-group-members-popup />
+    <user-added-popup />
+    <router-view />
+  </main>
+</template>
+
+<script>
+export default {
+  components: {
+    /**
+     * Resolves components asynchronously
+     * @see {@link https://vuejs.org/v2/guide/components-dynamic-async.html|Async Components}
+     */
+    AppHeader: () => import("@/desktop/components/common/AppHeader"),
+    AddNewContactPopup: () =>
+      import("@/desktop/components/popups/AddNewContactPopup"),
+    WeatherPopup: () => import("@/desktop/components/popups/WeatherPopup"),
+    CreateGroupPopup: () =>
+      import("@/desktop/components/popups/CreateGroupPopup"),
+    EditGroupPopup: () => import("@/desktop/components/popups/EditGroupPopup"),
+    AddGroupMembersPopup: () =>
+      import("@/desktop/components/popups/AddGroupMembersPopup"),
+    UserAddedPopup: () => import("@/desktop/components/popups/UserAddedPopup"),
+    FullScreenImagePopup: () =>
+      import("@/desktop/components/popups/FullScreenImagePopup"),
+    Notifier: () => import("@/shared/components/Notifier")
+  },
+  watch: {
+    /**
+     * Perform init action when socket connected
+     */
+    isConnected() {
+      this.$store.dispatch("init");
+    }
+  },
+  computed: {
+    /* Socket connection status */
+    isConnected() {
+      return this.$store.state.isSocketConnected;
+    },
+
+    /* Is header displayed */
+    isHeader() {
+      return this.$route.meta.isHeader;
+    },
+
+    /* Is overlay visible */
+    isAppOverlayed() {
+      return this.$store.getters.isAnyPopupVisible;
+    }
+  },
+  async beforeCreate() {
+    await this.$lib.getAppConfig();
+
+    /**
+     * Opens socket connection and defines
+     * list of callbacks, that used in store
+     * to invoke actions
+     */
+    await this.$socket.open([
+      "getGroupAll",
+      "getGroupInfo",
+      "getMessageById",
+      "getContacts",
+      "getOtherInfo",
+      "getGroupMembersById",
+      "getBefore",
+      "postGroup",
+      "deleteGroupById",
+      "notification",
+      "searchByName",
+      "getPrivacy"
+    ]);
+
+    /* Saves connection to store */
+    this.$store.commit("SET_SOCKET_CONNECTION_STATE", true);
+  }
+};
+</script>
+
+
+<style lang="stylus" src="@/shared/styles/app.styl" />
