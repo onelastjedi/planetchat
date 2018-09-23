61155b6644c1a96639fc96fbc186108c28a63cbf
Anton Komarenko <mi3ta@sent.as>
2018-09-23 08:51:16 +0300

    :bug: Fixes bug with getting endpoints configurations

--- src/desktop/main.js
+++ src/desktop/main.js
@@ -5,30 +5,23 @@ import store from "@/shared/store/store";
 import socket from "@/shared/utils/socket.io";
 import lib from "@/shared/utils/lib";
 import _ from "lodash";
+/**
+ * Gives global access to socket inside components with this.$socket
+ */
+Vue.prototype.$socket = socket;
 
-const init = async () => {
-  if (await lib.getAppConfig()) {
-    /**
-     * Gives global access to socket inside components with this.$socket
-     */
-    Vue.prototype.$socket = socket;
+/* Gives global access to lib inside components with this.$lib */
+Vue.prototype.$lib = lib;
 
-    /* Gives global access to lib inside components with this.$lib */
-    Vue.prototype.$lib = lib;
+/* Gives global access to lodash inside components with this.$_ */
+Vue.prototype.$_ = _;
 
-    /* Gives global access to lodash inside components with this.$_ */
-    Vue.prototype.$_ = _;
+/* Disables hints */
+Vue.config.productionTip = false;
 
-    /* Disables hints */
-    Vue.config.productionTip = false;
-
-    /* Construct and mount new Vue app */
-    new Vue({
-      router,
-      store,
-      render: h => h(App)
-    }).$mount("#app");
-  }
-};
-
-init();
+/* Construct and mount new Vue app */
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
