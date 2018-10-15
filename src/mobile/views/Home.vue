e49b61b53885d50ed0923f129be288b6936a24e6
Anton Komarenko <mi3ta@sent.as>
2018-10-15 20:39:39 +0200

    :sparkles: Adds <Home> for mobile app

---
+++ src/mobile/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <chat-groups-wrapper />
+</template>
+
+<script>
+export default {
+  components: {
+    /**
+     * Resolves components asynchronously
+     * @see {@link https://vuejs.org/v2/guide/components-dynamic-async.html|Async Components}
+     */
+    ChatGroupsWrapper: () =>
+      import("@/mobile/components/groups/ChatGroupsWrapper")
+  }
+};
+</script>
